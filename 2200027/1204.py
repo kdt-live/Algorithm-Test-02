f479f794179eae7e93a04dc8fdcd4b902d8afa84
armadillo <dltngks54@gmail.com>
2023-01-20 17:32:29 +0900

    2200027

--- 2200027/1204.py
+++ 2200027/1204.py
@@ -2,8 +2,6 @@ t = int(input())
 
 for i in range(1, t+1):
     case_num = int(input())
-
-
     socore = list(map(int,input().split()))
     dic_socore = {}
     for j in range(len(socore)):
@@ -12,7 +10,6 @@ for i in range(1, t+1):
         except:
             dic_socore[socore[j]] = 1
 
-    lst_count = sorted(dic_socore.values(), reverse=True)
     print(f'#{i} {sorted(dic_socore.items(), key=lambda x: x[1], reverse=True)[0][0]}')
         
         
