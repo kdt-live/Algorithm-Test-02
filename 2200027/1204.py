38155a4b0d7fb1e982542e5569e889d20476c952
armadillo <dltngks54@gmail.com>
2023-01-20 17:27:48 +0900

    2200027

--- 2200027/1204.py
+++ 2200027/1204.py
@@ -2,20 +2,17 @@ t = int(input())
 
 for i in range(1, t+1):
     case_num = int(input())
+
+
     socore = list(map(int,input().split()))
-    answer = {}
+    dic_socore = {}
     for j in range(len(socore)):
         try:
-            answer[socore[j]] += 1
+            dic_socore[socore[j]] += 1
         except:
-            answer[socore[j]] = 1
-    count_list = sorted(answer.values(), reverse=True) #카운트수 역순으로 정렬
+            dic_socore[socore[j]] = 1
 
-    if count_list[0] == count_list[1]: #최빈값 중복되면
-        max_num = sorted(answer.keys(), reverse=True)[0]
-        print(f'#{i} {max_num}')
-    else:
-        for k, v in answer.items():
-            if v == count_list[0]:
-                print(f'#{i} {k}')
-            
+    lst_count = sorted(dic_socore.values(), reverse=True)
+    print(f'#{i} {sorted(dic_socore.items(), key=lambda x: x[1], reverse=True)[0][0]}')
+        
+        
--- 2200027/14649.py
+++ 2200027/14649.py
@@ -2,15 +2,15 @@ t = int(input())
 
 for i in range(1, t+1):
     nums = list(map(int, input().split()))
-    num_sum = 0
-    answer = 0
+    num_sum = 0 #숫자합
+    N = 0
     for j in range(len(nums)):
         if j % 2 == 0: #홀수
             num_sum += nums[j] * 2
         else:
             num_sum += nums[j]
-    if num_sum % 10 == 0:
-        answer = 0
+    if num_sum % 10 == 0: #숫자합이 10으로 나누어 떨어지면
+        N = 0
     else:
-        answer = 10 - (num_sum % 10)
-    print(f'#{i} {answer}')
+        N = 10 - (num_sum % 10)
+    print(f'#{i} {N}')
