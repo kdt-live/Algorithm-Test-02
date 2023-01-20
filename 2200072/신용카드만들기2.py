9c3b4bf76e357d79d45f99155c714acb815779b8
ming0830 <minsun0830@naver.com>
2023-01-20 17:27:17 +0900

    refactor: -가 섞여있는 카드번호를 공백 없이 만든 후 판별함

--- 2200072/신용카드만들기2.py
+++ 2200072/신용카드만들기2.py
@@ -1,18 +1,12 @@
 T = int(input())
 start_numbers = ['3','4','5','6','9']
 for i in range(T):
-  card_numbers = input().split('-')
+  card_numbers = input()
 
-  if len(card_numbers) == 1:
-    if len(list(card_numbers[0])) == 16 and list(card_numbers)[0][0] in start_numbers:
-      print(f'#{i+1} 1')
-    else:
-      print(f'#{i+1} 0')
-  elif len(card_numbers) == 4:
-    if card_numbers[0][0] in start_numbers:
-      print(f'#{i+1} 1')
-    else:
-      print(f'#{i+1} 0')
+  card_numbers_list = list(card_numbers.replace('-',''))
+
+  if len(card_numbers_list) == 16 and (card_numbers_list[0] in start_numbers):
+    print(f'#{i+1} 1')
   else:
     print(f'#{i+1} 0')
 
