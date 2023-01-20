ad65f7a493f2f19b779f388ef3187d523c742e6d
winterkang <8670303@naver.com>
2023-01-20 17:48:58 +0900

    upload

---
+++ 2200026/14654.py
@@ -0,0 +1,8 @@
+num = ['3', '4', '5', '6', '9']
+T = int(input())  # 테스트 케이스 수 : 10
+for t in range(1, T + 1):
+    card = input().split(' ')
+    # for i in card:
+    #     if i == num
+    print(card)
+# 하이픈 제거해야함
