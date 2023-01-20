0e793b70a82299af9f56ecb6c756442121d515fb
codesloth <djqmflsk@naver.com>
2023-01-20 15:19:12 +0900

    5회차_이민욱

---
+++ 2200021/14649.py
@@ -0,0 +1,16 @@
+# 신용카드 만들기 1
+
+import sys
+sys.stdin = open('input.txt','r')
+input = sys.stdin.readline
+
+for t in range(1,int(input())+1):
+    nums = list(map(int,input().split()))
+    total = 0
+    for i in range(15):
+        if i % 2 == 0:
+            total += nums[i] * 2   
+        else:
+            total += nums[i]     
+    n = (10 - (total % 10))%10
+    print(f'#{t} {n}')
