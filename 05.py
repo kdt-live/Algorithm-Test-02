7ef1201771ca02d81a8b0b83cd7d1e2f723e78ad
sunjin <changido2888@gmail.com>
2023-01-20 18:05:41 +0900

    test2

--- 05.py
+++ 05.py
@@ -2,14 +2,15 @@
 
 T= int(input())
 for t in range(1,T+1):
-    people = int(input())
-    money = map(int,input().split())
-    avg = sum(money) // people
+    p = int(input())
+    money = list(map(int,input().split()))
+    
+    avg = sum(money) // p
     count = 0
 
     for i in money:
         if i <= avg:
             count +=1
+
     print(f'#{t} {count}')
 
-# 맞는지는 모르겠어요 ㅎ ㅠ 시간이 없어서 값을 입력 못해봐ㅏㅆ어요 
