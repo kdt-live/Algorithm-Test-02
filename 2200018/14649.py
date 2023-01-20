88884129b6e2e5434e0dde95802e256a974d288c
thebjko <thebjko@gmail.com>
2023-01-20 16:39:47 +0900

    minor refactor

--- 2200018/14649.py
+++ 2200018/14649.py
@@ -11,5 +11,5 @@ for i in range(T):
     odd = sum(map(lambda x: x * 2, ls[::2]))
     even = sum(ls[1::2])
     
-    N = (odd + even) % 10
-    print(f"#{i + 1} {(10 - N) % 10}")
+    n = (odd + even) % 10
+    print(f"#{i + 1} {(10 - n) % 10}")
