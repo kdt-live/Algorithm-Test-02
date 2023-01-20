861147aef0076c8d25f7d3a72b0a4dba6ac2ae45
armadillo <dltngks54@gmail.com>
2023-01-20 17:27:48 +0900

    2200027

--- 2200027/10804.py
+++ 2200027/10804.py
@@ -6,12 +6,7 @@ for i in range(1, t+1):
     answer = []
     for j in range(len(text)):
         try:
-            answer.insert(0, mirror[text[j]])
+            answer.insert(0, mirror[text[j]]) #거울처럼 거꾸로 추가
         except:
             answer.append(mirror[text[j]])
-    print(f'#{i}', end=' ')
-    print(*answer, sep='')
-
-
-
-
+    print(f'#{i} ' + ''.join(answer))
