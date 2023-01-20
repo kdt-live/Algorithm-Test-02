7c934a666a4cbbe58aa6dc0f484e11edab9cfd5c
lemon-lime-honey <kim.bumseo@gmail.com>
2023-01-20 15:08:24 +0900

    Submit 2200046

---
+++ 2200046/신용카드 만들기1.py
@@ -0,0 +1,18 @@
+def luhn(n):
+    result = 0
+    
+    for i in range(1, 16):
+        if i % 2 != 0:
+            result += 2 * n[i - 1]
+        else:
+            result += n[i - 1]
+    
+    for i in range(10):
+        if (result + i) % 10 == 0:
+            return i
+
+t = int(input())
+
+for i in range(t):
+    number = list(map(int, input().split()))
+    print(f'#{i + 1} {luhn(number)}')
