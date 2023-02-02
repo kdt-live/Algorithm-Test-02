09be38fa84a26f635634896b0799a8bca868b0bd
jeongyun11 <121421398+jeongyun11@users.noreply.github.com>
2023-02-02 16:30:09 +0900

    Update 6_1204 . 최빈수 구하기.py

--- 2200075/6_1204 . 최빈수 구하기.py
+++ 2200075/6_1204 . 최빈수 구하기.py
@@ -9,8 +9,4 @@ for t in range(1,T + 1) :
     for number in numbers :
         dict_count100[number] = dict_count100.get(number,0) + 1
 
-    for number in dict_count100 :
-        if max(dict_count100.values()) == dict_count100[number] :
-            print(f'#{t} {number}')
-            break
-
+    print(f'#{t} {sorted(dict_count100.items(), key=lambda x : x[1], reverse = True)[0][0]}')
